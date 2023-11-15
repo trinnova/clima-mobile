836d2f28245597df3bfe6628eff2f771b5d3a799
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-15 13:15:51 -0600

    Creando el boton para enviar la peticion a la API

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, StyleSheet } from 'react-native';
+import { Text, View, TextInput, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 const Formulario = () => {
@@ -11,6 +11,8 @@ const Formulario = () => {
                     placeholderTextColor="#666"
                 />
             </View>
+
+            <View>
                 <Picker>
                     <Picker.Item label="-- Seleccione un País --" value="" />
                     <Picker.Item label="Estados Unidos" value="US" />
@@ -21,9 +23,13 @@ const Formulario = () => {
                     <Picker.Item label="España" value="ES" />
                     <Picker.Item label="Perú" value="PE" />
                 </Picker>
-            <View>
-
             </View>
+
+            <TouchableWithoutFeedback>
+                <View>
+                    <Text>Buscar Clima</Text>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     )
 };
