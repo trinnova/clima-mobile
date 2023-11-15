41bfbc0db3d19b86a7d8afba89abcec235845b05
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-15 17:53:44 -0600

    Cambiando el color de fondo de acuerdo a la temperatura

--- App.js
+++ App.js
@@ -18,9 +18,8 @@ const App = () => {
   });
 
   const [ consultar, guardarConsultar ] = useState(false);
-
   const [ resultado, guardarResultado ] = useState({});
-
+  const [ bgColor, guardarBgColor ] = useState('rgb(71, 149, 212)');
   const { ciudad, pais } = busqueda;
 
   useEffect( () => {
@@ -48,6 +47,25 @@ const App = () => {
 
           guardarResultado(resultado);
           guardarConsultar(false);
+
+          // Modifica el color de FONDO de acuerdo a la temperatura
+          const kelvin = 273.15;
+          const { main } = resultado;
+          const actual = main.temp - kelvin;
+
+          if (actual < 10)
+          {
+            guardarBgColor('rgb( 105, 108, 149)');
+          }
+          else if (actual >= 10 && actual < 25)
+          {
+            guardarBgColor('rgb(71, 149, 212)');
+          }
+          else
+          {
+            guardarBgColor('rgb( 178, 28, 61 )');
+          }
+
         }
         catch (error)
         {
@@ -77,10 +95,14 @@ const App = () => {
     Keyboard.dismiss();
   };
 
+  const bgColorApp = {
+    backgroundColor: bgColor
+  };
+
   return (
     <>
       <TouchableWithoutFeedback onPress={ () => ocultarTeclado() } >
-        <View style={styles.app}>
+        <View style={[styles.app, bgColorApp]}>
           <View style={styles.contenido}>
             <Clima 
               resultado={resultado}
@@ -103,7 +125,6 @@ const styles = StyleSheet.create
   {
     app: {
       flex: 1,
-      backgroundColor: 'rgb(71, 149, 212)',
       justifyContent: 'center',
     },
 
